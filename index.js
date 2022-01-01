65b6ba6326fe6677e743f9943ca298c690d07d1d
vaibhav tiwari <vaibhavtiwari334455@gmail.com>
2022-01-01 17:11:30 +0530

    final

--- index.js
+++ index.js
@@ -1,20 +1,23 @@
-
 const hoursElement = document.getElementById("hour_hand");
 const minutesElement = document.getElementById("minute_hand");
 const secondsElement = document.getElementById("second_hand");      // here we are accessing the DOM elements in variables for further 
 const textElement = document.getElementById("text_element");        // manipulations.
 let sound = document.getElementById("tick");
 
+setInterval(function(){sound.play();},10);                          // ticking sound is given using this function
+
 let showdate=true;
 function animate()
 {
     const date = new Date();
     const day = date.getDate();
+    const month = date.getMonth();
+    const year = date.getFullYear();
     ampm = date.gethours>12? "AM":"PM";
     const hour = date.getHours() + date.getMinutes() / 60;
     const minute = date.getMinutes() + date.getSeconds() / 60; 
     const second = date.getSeconds();
-    textElement.textContent = showdate ? day : ampm;
+    textElement.textContent = showdate ? `${day}-${month}-${year}`: `---${ampm}----`;
   hoursElement.setAttribute("transform", `rotate(${(360 / 12) * hour})`);
   minutesElement.setAttribute("transform", `rotate(${(360 / 60) * minute})`);  // this how you rotate the hands of the clocks.
   secondsElement.setAttribute("transform", `rotate(${(360 / 60) * second})`);
@@ -22,4 +25,3 @@ function animate()
 }
 requestAnimationFrame(animate);         
 textElement.addEventListener( "click" ,() =>{ showdate=!showdate; });
-
